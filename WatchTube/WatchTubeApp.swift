138e69d1ca549a5f30dfb6e9c3235ab4fea699f9
Rahmn-Dev <sidqi.rahman@gmail.com>
2026-06-05 13:57:50 +0700

    Initial Commit

---
+++ WatchTube/WatchTubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchTubeApp.swift
+//  WatchTube
+//
+//  Created by Macbook Aul on 05/06/26.
+//
+
+import SwiftUI
+
+@main
+struct WatchTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
